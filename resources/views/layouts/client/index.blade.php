28fdea24f5371f1354e81ec7b14db5a5b63ffd10
akhmadrizki <akhmadrizki24@gmail.com>
2023-04-17 00:49:05 +0800

    First blood

---
+++ resources/views/layouts/client/index.blade.php
@@ -0,0 +1,448 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Pod Talk - Free Bootstrap 5 CSS Template</title>
+
+    <!-- CSS FILES -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400&family=Sono:wght@200;300;400;500;700&display=swap"
+        rel="stylesheet">
+
+    <link rel="stylesheet" href="{{ asset('client-template/css/bootstrap.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('client-template/css/bootstrap-icons.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('client-template/css/owl.carousel.min.css') }}">
+
+    <link rel="stylesheet" href="{{ asset('client-template/css/owl.theme.default.min.css') }}">
+
+    <link href="{{ asset('client-template/css/templatemo-pod-talk.css') }}" rel="stylesheet">
+
+    <!--
+
+TemplateMo 584 Pod Talk
+
+https://templatemo.com/tm-584-pod-talk
+
+-->
+</head>
+
+<body>
+
+    <main>
+
+        <nav class="navbar navbar-expand-lg">
+            <div class="container">
+                <a class="navbar-brand me-lg-5 me-0" href="index.html">
+                    <h3>SiInfus</h3>
+                </a>
+
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav ms-lg-auto">
+                        <li class="nav-item">
+                            <a class="nav-link active" href="index.html">Home</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="about.html">About</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="contact.html">Contact</a>
+                        </li>
+                    </ul>
+
+                    <div class="ms-4">
+                        <a href="#section_3" class="btn custom-btn custom-border-btn smoothscroll">Get started</a>
+                    </div>
+                </div>
+            </div>
+        </nav>
+
+
+        <section class="hero-section">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="text-center mb-5 pb-2">
+                            <h1 class="text-dark">Listen to Pod Talk</h1>
+
+                            <p class="text-dark">Listen it everywhere. Explore your fav podcasts.</p>
+
+                            <a href="#section_2" class="btn custom-btn smoothscroll mt-3">Update Info Terbaru 👇</a>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <section class="latest-podcast-section section-padding pb-0" id="section_2">
+            <div class="container">
+                <div class="row justify-content-center">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="section-title-wrap mb-5">
+                            <h4 class="section-title">Informasi Terbaru</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-6 col-12 mb-4 mb-lg-0">
+                        <div class="custom-block d-flex">
+                            <div class="">
+                                <div class="custom-block-icon-wrap">
+                                    <div class="section-overlay"></div>
+                                    <a href="detail-page.html" class="custom-block-image-wrap">
+                                        <img src="images/podcast/11683425_4790593.jpg"
+                                            class="custom-block-image img-fluid" alt="">
+                                    </a>
+                                </div>
+
+                                <div class="mt-2">
+                                    <a href="#" class="btn custom-btn">
+                                        Detail
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="custom-block-info">
+                                <div class="custom-block-top d-flex mb-1">
+                                    <small class="me-4">
+                                        <i class="bi-clock-fill custom-icon"></i>
+                                        50 Minutes
+                                    </small>
+                                </div>
+
+                                <h5 class="mb-2">
+                                    <a href="detail-page.html">
+                                        Modern Vintage
+                                    </a>
+                                </h5>
+
+                                <div class="profile-block d-flex">
+                                    <img src="images/profile/woman-posing-black-dress-medium-shot.jpg"
+                                        class="profile-block-image img-fluid" alt="">
+
+                                    <p>
+                                        Elsa
+                                        <img src="images/verified.png" class="verified-image img-fluid" alt="">
+                                        <strong>Influencer</strong>
+                                    </p>
+                                </div>
+
+                                <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+                            </div>
+
+                            <div class="d-flex flex-column ms-auto">
+                                <a href="#" class="badge ms-auto">
+                                    <i class="bi bi-share"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <section class="topics-section section-padding pb-0" id="section_3">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="section-title-wrap mb-5">
+                            <h4 class="section-title">Topics</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                        <div class="custom-block custom-block-overlay">
+                            <a href="detail-page.html" class="custom-block-image-wrap">
+                                <img src="images/topics/physician-consulting-his-patient-clinic.jpg"
+                                    class="custom-block-image img-fluid" alt="">
+                            </a>
+
+                            <div class="custom-block-info custom-block-overlay-info">
+                                <h5 class="mb-1">
+                                    <a href="listing-page.html">
+                                        Productivity
+                                    </a>
+                                </h5>
+
+                                <p class="badge mb-0">50 Episodes</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                        <div class="custom-block custom-block-overlay">
+                            <a href="detail-page.html" class="custom-block-image-wrap">
+                                <img src="images/topics/repairman-doing-air-conditioner-service.jpg"
+                                    class="custom-block-image img-fluid" alt="">
+                            </a>
+
+                            <div class="custom-block-info custom-block-overlay-info">
+                                <h5 class="mb-1">
+                                    <a href="listing-page.html">
+                                        Technician
+                                    </a>
+                                </h5>
+
+                                <p class="badge mb-0">12 Episodes</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                        <div class="custom-block custom-block-overlay">
+                            <a href="detail-page.html" class="custom-block-image-wrap">
+                                <img src="images/topics/woman-practicing-yoga-mat-home.jpg"
+                                    class="custom-block-image img-fluid" alt="">
+                            </a>
+
+                            <div class="custom-block-info custom-block-overlay-info">
+                                <h5 class="mb-1">
+                                    <a href="listing-page.html">
+                                        Mindfullness
+                                    </a>
+                                </h5>
+
+                                <p class="badge mb-0">35 Episodes</p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-3 col-md-6 col-12 mb-4 mb-lg-0">
+                        <div class="custom-block custom-block-overlay">
+                            <a href="detail-page.html" class="custom-block-image-wrap">
+                                <img src="images/topics/delicious-meal-with-sambal-arrangement.jpg"
+                                    class="custom-block-image img-fluid" alt="">
+                            </a>
+
+                            <div class="custom-block-info custom-block-overlay-info">
+                                <h5 class="mb-1">
+                                    <a href="listing-page.html">
+                                        Cooking
+                                    </a>
+                                </h5>
+
+                                <p class="badge mb-0">12 Episodes</p>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+
+
+        <section class="trending-podcast-section section-padding">
+            <div class="container">
+                <div class="row">
+
+                    <div class="col-lg-12 col-12">
+                        <div class="section-title-wrap mb-5">
+                            <h4 class="section-title">Trending episodes</h4>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+                        <div class="custom-block custom-block-full">
+                            <div class="custom-block-image-wrap">
+                                <a href="detail-page.html">
+                                    <img src="images/podcast/27376480_7326766.jpg" class="custom-block-image img-fluid"
+                                        alt="">
+                                </a>
+                            </div>
+
+                            <div class="custom-block-info">
+                                <h5 class="mb-2">
+                                    <a href="detail-page.html">
+                                        Vintage Show
+                                    </a>
+                                </h5>
+
+                                <div class="profile-block d-flex">
+                                    <img src="images/profile/woman-posing-black-dress-medium-shot.jpg"
+                                        class="profile-block-image img-fluid" alt="">
+
+                                    <p>Elsa
+                                        <strong>Influencer</strong>
+                                    </p>
+                                </div>
+
+                                <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+
+                                <div class="custom-block-bottom d-flex justify-content-between mt-3">
+                                    <a href="#" class="bi-headphones me-1">
+                                        <span>100k</span>
+                                    </a>
+
+                                    <a href="#" class="bi-heart me-1">
+                                        <span>2.5k</span>
+                                    </a>
+
+                                    <a href="#" class="bi-chat me-1">
+                                        <span>924k</span>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="social-share d-flex flex-column ms-auto">
+                                <a href="#" class="badge ms-auto">
+                                    <i class="bi-heart"></i>
+                                </a>
+
+                                <a href="#" class="badge ms-auto">
+                                    <i class="bi-bookmark"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-12 mb-4 mb-lg-0">
+                        <div class="custom-block custom-block-full">
+                            <div class="custom-block-image-wrap">
+                                <a href="detail-page.html">
+                                    <img src="images/podcast/27670664_7369753.jpg" class="custom-block-image img-fluid"
+                                        alt="">
+                                </a>
+                            </div>
+
+                            <div class="custom-block-info">
+                                <h5 class="mb-2">
+                                    <a href="detail-page.html">
+                                        Vintage Show
+                                    </a>
+                                </h5>
+
+                                <div class="profile-block d-flex">
+                                    <img src="images/profile/cute-smiling-woman-outdoor-portrait.jpg"
+                                        class="profile-block-image img-fluid" alt="">
+
+                                    <p>
+                                        Taylor
+                                        <img src="images/verified.png" class="verified-image img-fluid" alt="">
+                                        <strong>Creator</strong>
+                                    </p>
+                                </div>
+
+                                <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+
+                                <div class="custom-block-bottom d-flex justify-content-between mt-3">
+                                    <a href="#" class="bi-headphones me-1">
+                                        <span>100k</span>
+                                    </a>
+
+                                    <a href="#" class="bi-heart me-1">
+                                        <span>2.5k</span>
+                                    </a>
+
+                                    <a href="#" class="bi-chat me-1">
+                                        <span>924k</span>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="social-share d-flex flex-column ms-auto">
+                                <a href="#" class="badge ms-auto">
+                                    <i class="bi-heart"></i>
+                                </a>
+
+                                <a href="#" class="badge ms-auto">
+                                    <i class="bi-bookmark"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-4 col-12">
+                        <div class="custom-block custom-block-full">
+                            <div class="custom-block-image-wrap">
+                                <a href="detail-page.html">
+                                    <img src="images/podcast/12577967_02.jpg" class="custom-block-image img-fluid"
+                                        alt="">
+                                </a>
+                            </div>
+
+                            <div class="custom-block-info">
+                                <h5 class="mb-2">
+                                    <a href="detail-page.html">
+                                        Daily Talk
+                                    </a>
+                                </h5>
+
+                                <div class="profile-block d-flex">
+                                    <img src="images/profile/handsome-asian-man-listening-music-through-headphones.jpg"
+                                        class="profile-block-image img-fluid" alt="">
+
+                                    <p>
+                                        William
+                                        <img src="images/verified.png" class="verified-image img-fluid" alt="">
+                                        <strong>Vlogger</strong>
+                                    </p>
+                                </div>
+
+                                <p class="mb-0">Lorem Ipsum dolor sit amet consectetur</p>
+
+                                <div class="custom-block-bottom d-flex justify-content-between mt-3">
+                                    <a href="#" class="bi-headphones me-1">
+                                        <span>100k</span>
+                                    </a>
+
+                                    <a href="#" class="bi-heart me-1">
+                                        <span>2.5k</span>
+                                    </a>
+
+                                    <a href="#" class="bi-chat me-1">
+                                        <span>924k</span>
+                                    </a>
+                                </div>
+                            </div>
+
+                            <div class="social-share d-flex flex-column ms-auto">
+                                <a href="#" class="badge ms-auto">
+                                    <i class="bi-heart"></i>
+                                </a>
+
+                                <a href="#" class="badge ms-auto">
+                                    <i class="bi-bookmark"></i>
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    </main>
+
+
+    <footer class="site-footer text-center">
+        <p class="copyright-text mb-0 pt-4">Copyright &copy; {{ date('Y') }}</p>
+    </footer>
+
+
+    <!-- JAVASCRIPT FILES -->
+    <script src="{{ asset('client-template/js/jquery.min.js') }}"></script>
+    <script src="{{ asset('client-template/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('client-template/js/owl.carousel.min.js') }}"></script>
+    <script src="{{ asset('client-template/js/custom.js') }}"></script>
+
+</body>
+
+</html>
